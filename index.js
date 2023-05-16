ae334add0b899c49d8fb3738ca4e562dfa6d976c
Alejandro Cañas Arango <101745041+alejoc0314@users.noreply.github.com>
2023-05-16 17:38:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require('path');
 const hbs = require('hbs');
 
 const app = express();
-const port =3030;
 
 app.use ( express.static('public'));
 app.set("views",path.join(__dirname+ "/public/views"));
@@ -12,7 +11,3 @@ app.set("view engine","hbs");
 app.get('/', (req, res) => {
     res.render('disponibilidad');
   });
-
-app.listen(port,()=>{
-    console.log(`Escuchando el puerto ${port}`)
-})
